a9669ce67d6bc9ee8657ae803ce86df9419ece69
Shane Almocera <shaneerichalmocera@gmail.com>
2026-09-03 00:44:52 +0800

    Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+public class Activity {
+    public static void main(String[] args) {
+        /*String is used for data*/
+        String firstName = "Shane Erich";       /*stores the firstname*/
+        String lastName = "Almocera";           /*stores the lastname*/
+        String address = "Nueva Ecija";         /*stores the address*/
+        String mobileNumber = "09060480753";    /*stores the mobilenumber*/
+        /*int is used for whole numbers*/
+        int age = 18;
+        int gwa = 94;
+        /*displays all the informaton*/
+        System.out.println("Firstname: " + firstName);
+        System.out.println("Lastname: " + lastName);
+        System.out.println("Address:  " + address);
+        System.out.println("Mobilenumber: " + mobileNumber);
+        System.out.println("Age: " + age);
+        System.out.println("Gwa: " + gwa);
+
+    }
+}
